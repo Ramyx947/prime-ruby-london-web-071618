e64e33c14542cd0fa46b50010f00c72d40c081e7
Ramona M <r1mari@yahoo.com>
2018-07-05 17:07:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,34 @@
 # Add  code here!
 
-def prime?(n)
-  (2..n-1).each |x| 
-  if n%2 == 0
-    puts true 
-  else 
-  puts false
-end
-end
+# def prime?(n)
+#   (2..n-1).each |x| 
+#   if n%2 == 0
+#     puts true 
+#   else 
+#   puts false
+# end
+# end
 
+def prime?(num)
+
+  if num == 1
+    false
+  end
+  if num == 2
+    true
+  end
+  is_prime = true
+  for i in 2...num do
+    #puts "#{i}"
+    if num % i == 0
+      is_prime = false
+      break
+    end
+  end
+
+  is_prime
+
+end
 
     
   
